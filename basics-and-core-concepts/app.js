9c13d9e8cf33b70c0b70f22d241751af8d267928
TedEngineer <tedvu184@gmail.com>
2023-08-21 20:51:53 +1000

    explore function returning a function

--- basics-and-core-concepts/app.js
+++ basics-and-core-concepts/app.js
@@ -5,6 +5,16 @@ const app = Vue.createApp({
       vueLink: 'https://vuejs.org/',
     };
   },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return '<h1>Master React!</h1>';
+      } else {
+        return '<h1>Master Vue!</h1>';
+      }
+    },
+  },
 });
 
 app.mount('#user-goal');
